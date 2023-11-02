1d41d13d1e56621aaa53a0ff9b507f2780542e27
Aga Furgalska <aga.furgalska@wp.pl>
2023-11-02 18:54:07 +0100

    added EntryDisplay

--- src/pages/components/Entry.jsx
+++ src/pages/components/Entry.jsx
@@ -7,7 +7,6 @@ import { database } from "../../../firebase-config";
 import { ref, set, update } from "firebase/database";
 
 
-
 export function EntryCard({entry}){
     const { user } = UserAuth();
     const projects = GetProjects(user.uid);
@@ -16,6 +15,9 @@ export function EntryCard({entry}){
     const navigate = useNavigate();
     const [projectNames, setProjectNames] = useState([]);
     const [tagNames, setTagNames] = useState([]);
+
+    const [openEntryDisplay, setOpenEntryDisplay] = useState(false);
+
     
 
 
@@ -42,8 +44,6 @@ export function EntryCard({entry}){
     
     
     function handleTagClick(tag) {
-        const tagName = tag.name;
-        const tagID = tag.id;
         if (tag) {
             navigate(`/tags/${tag.id}/${tag.name}`);
         }
@@ -74,13 +74,16 @@ export function EntryCard({entry}){
 
 
     return(
-              <div key={entry.id} className="entryCard">
-                <h3>{entry.inputType}</h3>
-                <p>{entry.title}</p>
-                <p>{new Date(entry.timestamp).toLocaleDateString('en-GB')}</p>
-                <p>{entry.text}</p>
+              <div key={entry.id} className="entryCard" >
+                <div onClick={() => setOpenEntryDisplay(true)}>
+                    <h3>{entry.inputType}</h3>
+                    <p>{entry.title}</p>
+                    <p>{new Date(entry.timestamp).toLocaleDateString('en-GB')}</p>
+                    <p>{entry.text}</p>
+                </div>
+                
 
-                <p onClick={pinEntry}>{entry.pinned ? 'Pinned' : 'Pin'}</p>
+                <p onClick={() => pinEntry(entry, user)}>{entry.pinned === "true" ? 'Pinned' : 'Pin'}</p>
 
                 {/* Render tags as buttons */}
                 {entry.tags && (
@@ -106,10 +109,95 @@ export function EntryCard({entry}){
                 </div>
                 )}
 
+                <EntryDisplay open={openEntryDisplay} onClose={() => setOpenEntryDisplay(false)} entry={entry} projectNames={projectNames} tagNames={tagNames} />
+
               </div>
             );
 }
 
-export function EntryPreview(){
-    
+
+export function EntryDisplay({open, onClose, entry, projectNames, tagNames}){
+    if (!open) return null;
+    const navigate = useNavigate();
+
+
+    function handleTagClick(tag) {
+        if (tag) {
+            navigate(`/tags/${tag.id}/${tag.name}`);
+        }
+    }
+
+    function handleProjectClick(project) {
+        if (project) {
+            navigate(`/projects/${project.name}/${project.id}`);
+        }
+    }
+
+    function pinEntry() {
+        const entryRef = ref(database, `entries/${user.uid}/${entry.id}`);
+      
+        if (entry.pinned === true) {
+          update(entryRef, {
+            pinned: null
+          });
+          alert("Entry unpinned!");
+        } else {
+          update(entryRef, {
+            pinned: true
+          });
+          alert("Entry pinned!");
+        }
+      }
+
+    return(
+        <div className="overlay" onClick={onClose}>
+            <div key={entry.id} className="entryDisplay" onClick={(e => {e.stopPropagation();})}>
+                <button onClick={onClose}>x</button>
+                <h3>{entry.inputType}</h3>
+                <p>{entry.title}</p>
+                <p>{new Date(entry.timestamp).toLocaleDateString('en-GB')}</p>
+
+
+                {/* embed for spootify, musze wykombinowac modal do dodawania linkow do piosenek */}
+                {/* <iframe
+                style={{ borderRadius: "12px", width: "100%", height: "152px"}}
+                src="https://open.spotify.com/embed/track/3fpVWegR6YOS1Yk5HSMYIq?utm_source=generator&theme=0"
+                frameBorder="0"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+                ></iframe> */}
+
+                <p>{entry.text}</p>
+
+                <p onClick={() => pinEntry(entry, user)}>{entry.pinned === "true" ? 'Pinned' : 'Pin'}</p>
+
+                {/* Render tags as buttons */}
+                {entry.tags && (
+                <div>
+                    Tags:{" "}
+                    {tagNames.map((tag, index) => (
+                    <button key={index} onClick={() => handleTagClick(tag)}>
+                        {tag.name}
+                    </button>
+                    ))}
+                </div>
+                )}
+
+                {/* Render projects as buttons */}
+                {entry.projects &&(
+                <div>
+                    Projects:{" "}
+                    {projectNames.map((project, index) => (
+                    <button key={index} onClick={() => handleProjectClick(project)}>
+                        {project.name} {/* Render the 'name' property of the project */}
+                    </button>
+                    ))}
+                </div>
+                )}
+
+
+            </div>
+        </div>
+    )
 }
--- src/pages/components/EntryGroup.jsx
+++ src/pages/components/EntryGroup.jsx
@@ -41,71 +41,72 @@ export function GetEntries(userId) {
   return entries;
 }
 
-// function filterEntries(entries, filters) {
-//   const { pinned, keywords, date, inputType, tags, projects } = filters;
-//   if (!pinned && !keywords && !date && !inputType && !tags && !projects) {
-//     return entries; // Return all entries if no filters are provided
-//   }
-//   return entries.filter((entry) => {
-//     if (pinned && entry.pinned && entry.pinned === true){
-//       return true;
-//     }
-
-//     // Filter by keywords (searching for the same words in title and text)
-//     if (
-//       keywords &&
-//       (entry.title && entry.title.includes(keywords)) ||
-//       (entry.text && entry.text.includes(keywords))
-//     ) {
-//       return true;
-//     }
-//     // Filter by date (timestamp)
-//     if (date && new Date(entry.timestamp).toLocaleDateString('en-GB') === date) {
-//       return true;
-//     }
-//     // Filter by inputType
-//     if (inputType && entry.inputType === inputType) {
-//       return true;
-//     }
-//     if (tags && entry.tags && entry.tags.includes(tags)) {
-//       return true;
-//     }
-//     if (projects && entry.projects && entry.projects.includes(projects)) {
-//       return true;
-//     }
-//     return false;
-//   });
-// }
 
 function filterEntries(entries, filters) {
+  const { pinned, keywords, date, inputType, tags, projects } = filters;
+  if (!pinned && !keywords && !date && !inputType && !tags && !projects) {
+    return entries; // Return all entries if no filters are provided
+  }
   return entries.filter((entry) => {
-    const matchedPinned = filters.pinned !== null ? entry.pinned === filters.pinned : true;
-    const matchedKeywords = filters.keywords
-      ? entry.title.toLowerCase().includes(filters.keywords.toLowerCase()) ||
-        entry.content.toLowerCase().includes(filters.keywords.toLowerCase())
-      : true;
-    const matchedDate = filters.date
-      ? new Date(entry.timestamp).toLocaleDateString('en-GB') === filters.date
-      : true;
-    const matchedInputType = filters.inputType ? entry.inputType === filters.inputType : true;
-    let matchedTags = true;
-    let matchedProjects = true;
-
-    if (Array.isArray(filters.tags)) {
-      matchedTags = filters.tags.every(tag => entry.tags && entry.tags.includes(tag));
-    } else if (typeof filters.tags === 'string') {
-      matchedTags = entry.tags && entry.tags.includes(filters.tags);
+    if (pinned && entry.pinned && entry.pinned === true){
+      return true;
     }
-
-    if (Array.isArray(filters.projects)) {
-      matchedProjects = filters.projects.every(project => entry.projects && entry.projects.includes(project));
-    } else if (typeof filters.projects === 'string') {
-      matchedProjects = entry.projects && entry.projects.includes(filters.projects);
+    // Filter by keywords (searching for the same words in title and text)
+    if (
+      keywords &&
+      (entry.title && entry.title.includes(keywords)) ||
+      (entry.text && entry.text.includes(keywords))
+    ) {
+      return true;
     }
-
-    return matchedPinned && matchedKeywords && matchedDate && matchedInputType && matchedTags && matchedProjects;
+    // Filter by date (timestamp)
+    if (date && new Date(entry.timestamp).toLocaleDateString('en-GB') === date) {
+      return true;
+    }
+    // Filter by inputType
+    if (inputType && entry.inputType === inputType) {
+      return true;
+    }
+    if (tags && entry.tags && entry.tags.includes(tags)) {
+      return true;
+    }
+    if (projects && entry.projects && entry.projects.includes(projects)) {
+      return true;
+    }
+    return false;
   });
 }
+
+//RIGID FILTERING (excludes every entry that does not match) for library and pinned
+// function filterEntries(entries, filters) {
+//   return entries.filter((entry) => {
+//     const matchedPinned = filters.pinned !== null ? entry.pinned === filters.pinned : true;
+//     const matchedKeywords = filters.keywords
+//       ? entry.title.toLowerCase().includes(filters.keywords.toLowerCase()) ||
+//         entry.content.toLowerCase().includes(filters.keywords.toLowerCase())
+//       : true;
+//     const matchedDate = filters.date
+//       ? new Date(entry.timestamp).toLocaleDateString('en-GB') === filters.date
+//       : true;
+//     const matchedInputType = filters.inputType ? entry.inputType === filters.inputType : true;
+//     let matchedTags = true;
+//     let matchedProjects = true;
+
+//     if (Array.isArray(filters.tags)) {
+//       matchedTags = filters.tags.every(tag => entry.tags && entry.tags.includes(tag));
+//     } else if (typeof filters.tags === 'string') {
+//       matchedTags = entry.tags && entry.tags.includes(filters.tags);
+//     }
+
+//     if (Array.isArray(filters.projects)) {
+//       matchedProjects = filters.projects.every(project => entry.projects && entry.projects.includes(project));
+//     } else if (typeof filters.projects === 'string') {
+//       matchedProjects = entry.projects && entry.projects.includes(filters.projects);
+//     }
+
+//     return matchedPinned && matchedKeywords && matchedDate && matchedInputType && matchedTags && matchedProjects;
+//   });
+// }
 
 
 export function EntryGroup(props){

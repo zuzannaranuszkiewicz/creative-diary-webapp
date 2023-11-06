0a80c7b32baeb48ac255da079ca56e9b434ac8ea
Aga Furgalska <aga.furgalska@wp.pl>
2023-11-06 22:49:36 +0100

    sidebar

--- src/pages/components/SideBar.jsx
+++ src/pages/components/SideBar.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import '../../styles/sidebar.css';
 
 export function SideBar(){
 
-    const [opensSideMenu, setOpenSideMenu] = useState(false);
+    const [openSideMenu, setOpenSideMenu] = useState(false);
+
+    const toggleSideMenu = () => {
+      setOpenSideMenu(!openSideMenu);
+    };
 
     return(
         <>
-            <button onClick={opensSideMenu}>x</button>
-            <div>
-                <button onClick={() => setOpenSideMenu(false)}>x</button>
-                <nav>    
-                    <NavLink to="/dashboard/BrainDump">Dashboard</NavLink>
-                    <NavLink to="/library">Library</NavLink>
-                    <NavLink to="/calendar">Calendar</NavLink>
-                    <NavLink to="/account">Account</NavLink>
-                </nav>
-            </div>
-
-        </>
+        <button onClick={toggleSideMenu}>Toggle</button>
+  
+        <div className={`sidebar ${openSideMenu ? "open" : ""}`}>
+          <button onClick={toggleSideMenu}>x</button>
+          <nav>
+            <NavLink to="/dashboard/BrainDump">Dashboard</NavLink>
+            <NavLink to="/library">Library</NavLink>
+            <NavLink to="/calendar">Calendar</NavLink>
+            <NavLink to="/account">Account</NavLink>
+          </nav>
+        </div>
+      </>
         
     )
 
